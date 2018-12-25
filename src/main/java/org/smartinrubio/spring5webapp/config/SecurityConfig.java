acbc99468a46a1b34866ada5480089ef1fe108e2
Sergio Martin <econsergio@gmail.com>
2018-12-25 12:34:14 +0000

    add http restrictions

--- src/main/java/org/smartinrubio/spring5webapp/config/SecurityConfig.java
+++ src/main/java/org/smartinrubio/spring5webapp/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package org.smartinrubio.spring5webapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,5 +36,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .root("dc=smartinrubio,dc=org")
                 .ldif("classpath:users.ldif");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/hotels/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/user/{name:[\\d+]}").authenticated()
+                .anyRequest().permitAll();
     }
 }
