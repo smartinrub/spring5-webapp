ab15f459e76a51329590a61f71f2ef04ac949ba6
Sergio Martin <econsergio@gmail.com>
2019-01-06 21:18:05 +0000

    add jmx configuration

---
+++ src/main/java/org/smartinrubio/spring5webapp/config/JmxConfig.java
@@ -0,0 +1,10 @@
+package org.smartinrubio.spring5webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+
+@Configuration
+@EnableMBeanExport
+public class JmxConfig {
+
+}
--- src/main/java/org/smartinrubio/spring5webapp/controller/HotelController.java
+++ src/main/java/org/smartinrubio/spring5webapp/controller/HotelController.java
@@ -2,8 +2,10 @@ package org.smartinrubio.spring5webapp.controller;
 
 import org.smartinrubio.spring5webapp.exception.HotelNotFoundException;
 import org.smartinrubio.spring5webapp.model.Hotel;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +17,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.stream.Collectors;
 
+@Scope("prototype")
 @Controller
 @RequestMapping("/hotels")
+@ManagedResource(objectName = "hotel:name=HotelController")
 public class HotelController {
+
+    private static final int DEFAULT_HOTELS_PER_PAGE = 10;
+
+    private int hotelsPerPage = DEFAULT_HOTELS_PER_PAGE;
+
+    @ManagedAttribute
+    public int getHotelsPerPage() {
+        return hotelsPerPage;
+    }
+
+    @ManagedAttribute
+    public void setHotelsPerPage(int hotelsPerPage) {
+        this.hotelsPerPage = hotelsPerPage;
+    }
 
     private JpaRepository jpaRepository;
 
@@ -28,7 +47,8 @@ public class HotelController {
 
     @GetMapping
     public String getHotels(Model model) {
-        model.addAttribute(jpaRepository.findAll());
+        System.out.println(hotelsPerPage);
+        model.addAttribute(jpaRepository.findAll().stream().limit(getHotelsPerPage()).collect(Collectors.toList()));
         return "hotels";
     }
 
